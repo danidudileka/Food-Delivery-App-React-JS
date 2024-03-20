92c61a1b79704f191b23ecb4f28eefaee784478e
Danidu Dileka Perera <danidu1@yahoo.com>
2024-03-20 20:35:33 +0530

    Explore Menu img category

--- src/components/ExploreMenu/ExploreMenu.jsx
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -2,21 +2,32 @@ import React from "react";
 import "./ExploreMenu.css";
 import { menu_list } from "../../assets/assets";
 
-const ExploreMenu = () => {
+const ExploreMenu = ({ category, setCategory }) => {
   return (
     <div className="explore-menu" id="explore-menu">
       <h1>Explore Our Menu</h1>
       <p className="explore-menu-text">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima,
-        laboriosam dolorum illum quibusdam tempore nemo pariatur laborum dolor.
-        Sunt iusto distinctio quaerat qui laboriosam temporibus molestiae magni,
-        molestias rerum doloribus?
+        Choose from a diverse menu featuring a delectable array of dishes. Our
+        mission is to satisfy your cravings and elevate your dining experience,
+        one delicious meal at a time.
       </p>
       <div className="explore-menu-list">
         {menu_list.map((item, index) => {
           return (
-            <div key={index} className="explore-menu-list-item">
-              <img src={item.menu_image} alt="menu_image" />
+            <div
+              key={index}
+              className="explore-menu-list-item"
+              onClick={() =>
+                setCategory((prev) =>
+                  prev === item.menu_name ? "All" : item.menu_name
+                )
+              }
+            >
+              <img
+                src={item.menu_image}
+                className={category === item.menu_name ? "active" : ""}
+                alt="menu_image"
+              />
               <p>{item.menu_name}</p>
             </div>
           );
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
-import './Home.css'
-import Header from '../../components/Header/Header'
-import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
+import React, { useState } from "react";
+import "./Home.css";
+import Header from "../../components/Header/Header";
+import ExploreMenu from "../../components/ExploreMenu/ExploreMenu";
 
 const Home = () => {
+  const [category, setCategory] = useState("All");
+
   return (
     <div>
-      <Header/>
-      <ExploreMenu/>
+      <Header />
+      <ExploreMenu category={category} setCategory={setCategory} />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
