86989a0207618999a7b387e913c95b0c146fdb61
Danidu Dileka Perera <danidu1@yahoo.com>
2024-03-20 17:44:09 +0530

    Navbar completed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
+import { assets } from '../../assets/assets'
 
 const Navbar = () => {
+
+    const [menu,setMenu] = useState("home")
+
   return (
-    <div className='navbar'>Navbar</div>
+    <div className='navbar'>
+        <img src={assets.logo} alt="logo" />
+        <ul className="navbar-menu">
+            <li onClick={() => setMenu("home")} className={menu==='home' ? 'active' : ''}>Home</li>
+            <li onClick={() => setMenu("menu")} className={menu==='menu' ? "active" : ""}>Menu</li>
+            <li onClick={() => setMenu("mobile-app")} className={menu==='mobile-app' ? "active" : ""}>Mobile App</li>
+            <li onClick={() => setMenu("contact-us")} className={menu==='contact-us' ? "active" : ""}>Contact Us</li>
+        </ul>
+        <div className="navbar-right">
+            <img src={assets.search_icon} alt="search_icon" />
+            <div className="navbar-search-icon">
+                <img src={assets.basket_icon} alt="basket_icon" />
+                <div className="dot"></div>
+            </div>
+            <button>Sign in</button>
+        </div>
+    </div>
   )
 }
 
