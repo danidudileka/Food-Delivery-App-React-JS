f6a6fed4628b75d7770a6fe180ba0a612e54b181
Danidu Dileka Perera <danidu1@yahoo.com>
2024-03-20 18:23:29 +0530

    Header Section

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-contents">
+        <h2>Order Your Favourite Food Here</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi exercitationem eligendi vitae, laboriosam mollitia corporis explicabo, doloremque voluptatibus dolore laborum sint distinctio hic. Iure, laudantium sequi suscipit cumque id magni.</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import './Home.css'
+import Header from '../../components/Header/Header'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <Header/>
+    </div>
   )
 }
 
