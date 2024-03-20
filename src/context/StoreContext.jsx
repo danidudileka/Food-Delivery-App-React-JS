70fbdc4d0f25704123ad14242c1dd00a359d253d
Danidu Dileka Perera <danidu1@yahoo.com>
2024-03-20 23:01:53 +0530

    add & remove from cart context api

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -1,12 +1,30 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import { food_list } from "../assets/assets";
 
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
+  const [cartItems, setCartItems] = useState({});
+
+  const addToCart = (itemId) => {
+    if (!cartItems[itemId]) {
+      setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
+    } else {
+      setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+    }
+  };
+
+  const removeFromCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+  };
+
   const contextValue = {
-    food_list
-  }
+    food_list,
+    cartItems,
+    setCartItems,
+    addToCart,
+    removeFromCart,
+  };
 
   return (
     <StoreContext.Provider value={contextValue}>
@@ -15,4 +33,4 @@ const StoreContextProvider = (props) => {
   );
 };
 
-export default StoreContextProvider
+export default StoreContextProvider;
