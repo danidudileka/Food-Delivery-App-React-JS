14edecc8ed6d8c681474b3a4df9e54cd8ec26c0d
Danidu Dileka Perera <danidu1@yahoo.com>
2024-03-20 22:51:58 +0530

    add to cart item counter

--- src/components/FoodItem/FoodItem.jsx
+++ src/components/FoodItem/FoodItem.jsx
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "./FoodItem.css";
 import { assets } from "../../assets/assets";
 
 const FoodItem = ({ id, name, price, description, image }) => {
+  const [itemCount, setItemCount] = useState(0);
   return (
     <div className="food-item">
       <div className="food-item-img-container">
         <img src={image} alt="image" className="food-item-img" />
+        {!itemCount ? (
+          <img
+            src={assets.add_icon_white}
+            alt="add_icon_white"
+            className="add"
+            onClick={() => setItemCount((prev) => prev + 1)}
+          />
+        ) : (
+          <div className="food-item-counter">
+            <img
+              src={assets.remove_icon_red}
+              alt="remove_icon_red"
+              onClick={() => setItemCount((prev) => prev - 1)}
+            />
+            <p>{itemCount}</p>
+            <img
+              src={assets.add_icon_green}
+              alt="add_icon_green"
+              onClick={() => setItemCount((prev) => prev + 1)}
+            />
+          </div>
+        )}
       </div>
       <div className="food-item-info">
         <div className="food-item-name-rating">
