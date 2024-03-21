76e3a7cf3c32b273756f4e0a2f53688b523c69f3
Danidu Dileka Perera <danidu1@yahoo.com>
2024-03-21 09:21:12 +0530

    Food Items

--- src/components/FoodDisplay/FoodDisplay.jsx
+++ src/components/FoodDisplay/FoodDisplay.jsx
@@ -10,16 +10,18 @@ const FoodDisplay = ({ category }) => {
       <h2>Top Dishes Near You</h2>
       <div className="food-display-list">
         {food_list.map((item, index) => {
-          return (
-            <FoodItem
-              key={index}
-              id={item._id}
-              name={item.name}
-              description={item.description}
-              price={item.price}
-              image={item.image}
-            />
-          );
+          if ((category === "All" || category === item.category)) {
+            return (
+              <FoodItem
+                key={index}
+                id={item._id}
+                name={item.name}
+                description={item.description}
+                price={item.price}
+                image={item.image}
+              />
+            );
+          }
         })}
       </div>
     </div>
