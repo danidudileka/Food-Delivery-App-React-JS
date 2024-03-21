6abfa0f1543a325815a2bf7745fb80693e044647
Danidu Dileka Perera <danidu1@yahoo.com>
2024-03-21 12:12:56 +0530

    Header responsive css

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,13 +3,18 @@ import "./Header.css";
 
 const Header = () => {
   return (
-    <div className="header">
+    <header>
       <div className="header-contents">
         <h2>Order Your Favourite Food Here</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi exercitationem eligendi vitae, laboriosam mollitia corporis explicabo, doloremque voluptatibus dolore laborum sint distinctio hic. Iure, laudantium sequi suscipit cumque id magni.</p>
+        <p>
+          Choose from a diverse menu featuring a delectable array of dishes
+          crafted with the finest ingredients and culinary expertise. Our
+          mission is to satisfy your cravings and elevate your dining
+          experience, one delicious meal at a time.
+        </p>
         <button>View Menu</button>
       </div>
-    </div>
+    </header>
   );
 };
 
