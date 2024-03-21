d3638fc0535df6a1a1af416b539f96ec6e520764
Danidu Dileka Perera <danidu1@yahoo.com>
2024-03-21 18:06:44 +0530

    Login Popup

---
+++ src/components/LoginPopup/LoginPopup.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import "./LoginPopup.css";
+import { assets } from "../../assets/assets";
+
+const LoginPopup = ({ setShowLogin }) => {
+  const [currentState, setCurrentState] = useState("Sign up");
+  return (
+    <div className="login-popup">
+      <form className="login-popup-container">
+        <div className="login-popup-title">
+          <h2>{currentState}</h2>
+          <img
+            src={assets.cross_icon}
+            alt="cross_icon"
+            onClick={() => setShowLogin(false)}
+          />
+        </div>
+        <div className="login-popup-inputs">
+          {currentState === "Login" ? (
+            <></>
+          ) : (
+            <input type="text" placeholder="Your name" required />
+          )}
+          <input type="email" placeholder="Your email" required />
+          <input type="password" placeholder="Password" required />
+        </div>
+
+        <button>
+          {currentState === "Sign up" ? "Create Account" : "Login"}
+        </button>
+
+        {currentState === "Sign up" ? (
+          <div className="login-popup-condition">
+            <input type="checkbox" required />
+            <p>By continuing, I agree to the terms of use & privacy policy</p>
+          </div>
+        ) : (
+          <></>
+        )}
+
+        {currentState === "Login" ? (
+          <p>
+            Create a new account?
+            <span onClick={() => setCurrentState("Sign up")}>Click here</span>
+          </p>
+        ) : (
+          <p>
+            Already have an account?
+            <span onClick={() => setCurrentState("Login")}>Login here</span>
+          </p>
+        )}
+      </form>
+    </div>
+  );
+};
+
+export default LoginPopup;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import './Navbar.css'
 import { assets } from '../../assets/assets'
 import { Link } from 'react-router-dom'
 
-const Navbar = () => {
+const Navbar = ({setShowLogin}) => {
 
     const [menu,setMenu] = useState("home")
 
@@ -22,7 +22,7 @@ const Navbar = () => {
                 <img src={assets.basket_icon} alt="basket_icon" />
                 <div className="dot"></div>
             </div>
-            <button>Sign in</button>
+            <button onClick={() => setShowLogin(true)}>Sign in</button>
         </div>
     </div>
   )
